2b0f8b4fd64b22f0b5d9864f4cb51e17451ce88b
rida sersif <ridasersif1@gmail.com>
2025-10-30 16:52:19 +0100

    add CustomerDto class for customer delivery information

---
+++ src/main/java/org/supplychain/supplychain/dto/modelDelivery/CustomerDto.java
@@ -0,0 +1,12 @@
+package org.supplychain.supplychain.dto.modelDelivery;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+}
