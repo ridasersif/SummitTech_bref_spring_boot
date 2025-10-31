ec6ac40251dc8bb312cf636e697c5d6a661a0e03
rida sersif <ridasersif1@gmail.com>
2025-10-31 14:54:25 +0100

    add exceptions and handler

--- src/main/java/org/supplychain/supplychain/exception/DuplicateResourceException.java
+++ src/main/java/org/supplychain/supplychain/exception/DuplicateResourceException.java
@@ -1,7 +1,12 @@
 package org.supplychain.supplychain.exception;
 
 public class DuplicateResourceException extends RuntimeException {
+
     public DuplicateResourceException(String message) {
         super(message);
+    }
+
+    public DuplicateResourceException(String resource, String field, String value) {
+        super(String.format("%s already exists with %s: %s", resource, field, value));
     }
 }
