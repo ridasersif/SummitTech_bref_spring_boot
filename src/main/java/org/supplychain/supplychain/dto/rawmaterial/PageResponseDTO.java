505af17da7b4364d44df4ea25a48caba2639d141
rida sersif <ridasersif1@gmail.com>
2025-10-31 14:53:45 +0100

    add request and response dtos for rawmaterial

--- src/main/java/org/supplychain/supplychain/dto/rawmaterial/PageResponseDTO.java
+++ src/main/java/org/supplychain/supplychain/dto/rawmaterial/PageResponseDTO.java
@@ -1,4 +1,31 @@
 package org.supplychain.supplychain.dto.rawmaterial;
 
-public class PageResponseDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponseDTO<T> {
+
+    private List<T> content;
+
+    private int pageNumber;
+
+    private int pageSize;
+
+    private long totalElements;
+
+    private int totalPages;
+
+    private boolean first;
+
+    private boolean last;
+
+    private boolean hasNext;
+
+    private boolean hasPrevious;
 }
--- src/main/java/org/supplychain/supplychain/dto/rawmaterial/RawMaterialSummaryDTO.java
+++ src/main/java/org/supplychain/supplychain/dto/rawmaterial/RawMaterialSummaryDTO.java
@@ -1,4 +1,25 @@
 package org.supplychain.supplychain.dto.rawmaterial;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RawMaterialSummaryDTO {
+
+    private Long id;
+
+    private String name;
+
+    private Integer stock;
+
+    private Integer availableStock;
+
+    private Integer stockMin;
+
+    private String unit;
+
+    private Boolean isCriticalStock;
 }
