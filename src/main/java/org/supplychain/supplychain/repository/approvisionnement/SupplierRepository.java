94f0e1e54e3418a596308992b0dda12906961986
rida sersif <ridasersif1@gmail.com>
2025-10-31 14:55:11 +0100

    add repositories for raw materials

--- src/main/java/org/supplychain/supplychain/repository/approvisionnement/SupplierRepository.java
+++ src/main/java/org/supplychain/supplychain/repository/approvisionnement/SupplierRepository.java
@@ -1,4 +1,9 @@
 package org.supplychain.supplychain.repository.approvisionnement;
 
-public class SupplierRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.supplychain.supplychain.model.Supplier;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
 }
