8a05276355e5621b03f117fda13091524eeefdf9
rida sersif <ridasersif1@gmail.com>
2025-10-31 14:55:42 +0100

    add services for raw material

--- src/main/java/org/supplychain/supplychain/service/approvisionnement/RawMaterialService.java
+++ src/main/java/org/supplychain/supplychain/service/approvisionnement/RawMaterialService.java
@@ -1,4 +1,30 @@
-package org.supplychain.supplychain.service;
+package org.supplychain.supplychain.service.approvisionnement;
 
-public class RawMaterialService {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.supplychain.supplychain.dto.rawmaterial.RawMaterialRequestDTO;
+import org.supplychain.supplychain.dto.rawmaterial.RawMaterialResponseDTO;
+import org.supplychain.supplychain.dto.rawmaterial.RawMaterialSummaryDTO;
+
+import java.util.List;
+
+public interface RawMaterialService {
+
+    RawMaterialResponseDTO createRawMaterial(RawMaterialRequestDTO requestDTO);
+
+    RawMaterialResponseDTO updateRawMaterial(Long id, RawMaterialRequestDTO requestDTO);
+
+    void deleteRawMaterial(Long id);
+
+    RawMaterialResponseDTO getRawMaterialById(Long id);
+
+    Page<RawMaterialSummaryDTO> getAllRawMaterials(Pageable pageable);
+
+    Page<RawMaterialSummaryDTO> searchRawMaterials(String name, Pageable pageable);
+
+    List<RawMaterialSummaryDTO> getCriticalStockMaterials();
+
+    Page<RawMaterialSummaryDTO> getCriticalStockMaterialsWithPagination(Pageable pageable);
+
+    boolean isMaterialUsed(Long id);
 }
